d81cdc2422274e5bb2c47cc9ff678f87ce09bc0e
zealotpz <zealotpz@qq.com>
2015-12-17 16:42:41 +0800

    add sbm

---
+++ sbm/oracle/cleanSQL.sql
@@ -0,0 +1,8 @@
+--清空汇总表记录
+truncate table T_SBM_MT_BIKE_BIKE_MONTH;
+truncate table T_SBM_MT_BIKE_BIKE_YEAR;
+truncate table T_SBM_MT_BIKE_DAY;
+truncate table T_SBM_MT_BIKE_DEV_DAY;
+truncate table T_SBM_MT_BIKE_DEV_MONTH;
+truncate table T_SBM_MT_BIKE_USER_MONTH;
+truncate table T_SBM_MT_BIKE_USER_YEAR;
