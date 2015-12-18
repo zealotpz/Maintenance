3ebc736dbb5cecb89ed643aec50b4859391b61e1
zealotpz <zealotpz@qq.com>
2015-12-18 21:53:31 +0800

    Update publish_bikemt.sh
    fix some error

--- shellCode/publish_bikemt.sh
+++ shellCode/publish_bikemt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-bikemt_SDIR=/home/bikemt/apache-tomcat-bikemt
+BIKEMT_SDIR=/home/bikemt/apache-tomcat-bikemt
 StartTomcat=/home/bikemt/apache-tomcat-bikemt/bin/
 #获取时间并格式化
 time=`date +"%Y%m%d%H%M" `
@@ -24,13 +24,13 @@ sleep 3
 
 echo "-------------------  开始备份并删除日志文件与旧 war 包  ----------------------";
 #拷贝文件至至war_backup目录，没有则新建目录
-mkdir -p /home/bikemt/war_backup/ && cp ${BIKECC_SDIR}/webapps/bikemt.war /home/bikemt/war_backup/$filename
+mkdir -p /home/bikemt/war_backup/ && cp ${BIKEMT_SDIR}/webapps/bikemt.war /home/bikemt/war_backup/$filename
 echo "-----------------------   备份成功！  ---------------------------"
 rm -rf ${bikemt_SDIR}/logs/*
 rm -rf ${bikemt_SDIR}/bin/logs/*
 rm -rf ${bikemt_SDIR}/webapps/*
 echo "-----------------------   删除成功！  ---------------------------"
-cp /home/bikemt/war_new/whbike.war ${bikemt_SDIR}/webapps/
+cp /home/bikemt/war_new/bikemt.war ${BIKEMT_SDIR}/webapps/
 echo "-----------------------   新 war 拷贝成功！  --------------------------"
 echo "-----------------------   开始启动 bikemt Tomcat  --------------------------"
 #获取环境变量，否则提示找不到 NEST_HOME
