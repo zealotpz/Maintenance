b0cf4f7e80b4d40007030f2a004cc07031263281
zealotpz <zealotpz@qq.com>
2015-12-18 19:08:07 +0800

    Update publish_bikecc.sh
    add backup
    新增备份，获取环境变量及后台启动 tomcat

--- shellCode/publish_bikecc.sh
+++ shellCode/publish_bikecc.sh
@@ -2,6 +2,10 @@
 #!/bin/bash
 BIKECC_SDIR=/home/bike/apache-tomcat-bikecc
 StartTomcat=/home/bike/apache-tomcat-bikecc/bin/
+#获取时间并格式化
+time=`date +"%Y%m%d%H%M" `
+#备份文件名
+filename=bikecc_$time.war
 #获取 tomcat 进程并杀掉
 TomcatPID=$(ps -ef |grep tomcat |grep -w 'apache-tomcat-bikecc'|grep -v 'grep'|awk '{print $2}')
 if [ "$TomcatPID" = "" ]
@@ -20,7 +24,8 @@ echo "tomcat 关闭成功";
 sleep 3
 
 echo "-------------------  开始备份并删除日志文件与旧 war 包  ----------------------";
-cp ${BIKECC_SDIR}/webapps/*.war /home/bike/war_backup
+#拷贝文件至至war_backup目录，没有则新建目录
+mkdir -p /home/bikecc/war_backup/ && cp ${BIKECC_SDIR}/webapps/bikecc.war /home/bikecc/war_backup/$filename
 echo "-----------------------   备份成功！  ---------------------------"
 rm -rf ${BIKECC_SDIR}/logs/*
 rm -rf ${BIKECC_SDIR}/bin/logs/*
@@ -29,8 +34,9 @@ echo "-----------------------   删除成功！  ---------------------------"
 cp /home/bike/war_new/whbike.war ${BIKECC_SDIR}/webapps/
 echo "-----------------------   新 war 拷贝成功！  --------------------------"
 echo "-----------------------   开始启动 bikecc Tomcat  --------------------------"
+export NEST_HOME=/home/bike/NEST_FILE
 cd $StartTomcat
-nohup ./catalina.sh start &
+nohup ./startup.sh &
 sleep 4 #等候4秒
 TomcatNEWID=$(ps -ef |grep tomcat |grep -w 'apache-tomcat-bikecc'|grep -v 'grep'|awk '{print $2}')
 echo "bikecc tomcat 新进程为:$TomcatNEWID"
